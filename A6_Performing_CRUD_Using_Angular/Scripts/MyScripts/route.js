6a3dfc637a9335485a68c9dea4a02d019dff0b90
yatin sharma <sharmayatin489@gmail.com>
2015-03-24 11:27:01 +0530

    sample

---
+++ A6_Performing_CRUD_Using_Angular/Scripts/MyScripts/route.js
@@ -0,0 +1,17 @@
+﻿app.config(function ($routeProvider) {
+
+    $routeProvider.when("/Admin", {
+        templateUrl: "/Employee/Index",
+        controller: "crudController"
+    }
+    )
+    $routeProvider.when("/User", {
+        templateUrl: "/Employee/User",
+        controller: "crudController"
+    }
+)
+        .otherwise("/Admin");
+
+    
+
+});
